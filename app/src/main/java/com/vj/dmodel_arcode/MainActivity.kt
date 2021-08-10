a51021c9030854c7d6fc316f068fed582f7a4a7d
Vijay Vimalananda Guru <vg4029@gmail.com>
2021-08-10 11:16:25 +0530

    Initial commit

---
+++ app/src/main/java/com/vj/dmodel_arcode/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.vj.dmodel_arcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun initialize() {
+        TODO("Not yet implemented")
+    }
+
+
+    private fun isDeviceSupported(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
